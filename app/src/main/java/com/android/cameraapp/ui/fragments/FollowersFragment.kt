4ca1d88e74f3ca4af351753789b26074e872c2d3
Danielius <dnlspasin@gmail.com>
2019-10-28 21:05:37 +0200

    Followers fragment with layout

---
+++ app/src/main/java/com/android/cameraapp/ui/fragments/FollowersFragment.kt
@@ -0,0 +1,23 @@
+package com.android.cameraapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.cameraapp.databinding.FollowersFragmentBinding
+import com.android.cameraapp.databinding.LikesFragmentBinding
+import com.android.cameraapp.ui.adapters.LikesAdapter
+
+class FollowersFragment : Fragment() {
+    lateinit var binding: FollowersFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FollowersFragmentBinding.inflate(inflater, container, false)
+        binding.followersRecyclerView.adapter = LikesAdapter()
+        return binding.root
+    }
+}
