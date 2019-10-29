54190d76248cefbc6f29df63a69ab0418491e840
Danielius <dnlspasin@gmail.com>
2019-10-29 12:47:56 +0200

    Firebase Auth Module

---
+++ app/src/main/java/com/android/cameraapp/di/base_activity/FirebaseAuthModule.kt
@@ -0,0 +1,22 @@
+package com.android.cameraapp.di.base_activity
+
+import com.android.cameraapp.di.scopes.BaseActivityScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import dagger.Module
+import dagger.Provides
+
+@Module
+
+object FirebaseAuthModule {
+    @Provides
+    @JvmStatic
+    @BaseActivityScope
+    fun getFirebaseAuth() : FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @BaseActivityScope
+    @JvmStatic
+    fun getFirebaseCurrentUser(auth: FirebaseAuth) : FirebaseUser? = auth.currentUser
+
+}
--- app/src/main/java/com/android/cameraapp/ui/base_activity/BaseRerepository.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/BaseRerepository.kt
@@ -1,4 +1,9 @@
 package com.android.cameraapp.ui.base_activity
 
-class BaseRerepository {
+import com.android.cameraapp.di.scopes.BaseActivityScope
+import com.google.firebase.auth.FirebaseUser
+import javax.inject.Inject
+
+@BaseActivityScope
+class BaseRerepository @Inject constructor(val user:FirebaseUser) {
 }
