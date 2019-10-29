a9302fb0aadccee0bea07ddd9e699e5faa5b5290
Danielius <dnlspasin@gmail.com>
2019-10-29 14:09:35 +0200

    BaseViewModelModule// multinbinding into viewmodel map

---
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/BaseViewModelModule.kt
@@ -0,0 +1,17 @@
+package com.android.cameraapp.ui.base_activity
+
+import androidx.lifecycle.ViewModel
+import com.android.cameraapp.di.ViewModelKey
+import com.android.cameraapp.di.scopes.BaseActivityScope
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class BaseViewModelModule {
+    @Binds
+    @IntoMap
+    @BaseActivityScope
+    @ViewModelKey(BaseViewModel::class)
+    abstract fun baseViewModel(baseViewModel: BaseViewModel) : ViewModel
+}
