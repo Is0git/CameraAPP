96f384f5ca5a3000e8e922d38c3b5f8115321bf4
Danielius <dnlspasin@gmail.com>
2019-10-29 13:59:08 +0200

    Base Repository

--- app/src/main/java/com/android/cameraapp/ui/base_activity/BaseRerepository.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/BaseRerepository.kt
@@ -1,15 +1,42 @@
 package com.android.cameraapp.ui.base_activity
 
+import android.app.Application
 import androidx.navigation.NavController
+import com.android.cameraapp.R
 import com.android.cameraapp.di.scopes.BaseActivityScope
+import com.android.cameraapp.util.ToastHandler
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import javax.inject.Inject
 
 @BaseActivityScope
-class BaseRepository @Inject constructor(val user:FirebaseUser, val auth:FirebaseAuth, val navController: NavController) {
+class BaseRepository @Inject constructor(
+    val user: FirebaseUser,
+    val auth: FirebaseAuth,
+    val navController: NavController,
+    val application: Application
+) {
+    lateinit var listener: FirebaseAuth.AuthStateListener
 
-    fun logIn() {
-        auth.addAuthStateListener { if(it.currentUser.)}
+    init {
+        //Changing nav graphs depending on if user is logged in or not
+        listener = FirebaseAuth.AuthStateListener {
+            if (it.currentUser != null) navController.setGraph(R.navigation.auth_graph) else navController.setGraph(
+                R.navigation.nav
+            )
+            auth.addAuthStateListener { listener }
+
+        }
+
+    }
+
+    fun logIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { ToastHandler.showToast(application, "LOGGED IN") }
+            .addOnFailureListener { ToastHandler.showToast(application, "${it.message}") }
+    }
+
+    fun unsubscribeListener() {
+        auth.removeAuthStateListener(listener)
     }
 }
---
+++ app/src/main/java/com/android/cameraapp/util/ToastHandler.kt
@@ -0,0 +1,9 @@
+package com.android.cameraapp.util
+
+import android.app.Application
+import android.widget.Toast
+
+object ToastHandler {
+
+    fun showToast(application: Application, text:String="EMPTY TEXT") = Toast.makeText(application, text, Toast.LENGTH_LONG ).show()
+}
