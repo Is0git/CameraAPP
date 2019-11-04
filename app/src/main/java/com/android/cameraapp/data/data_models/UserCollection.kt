ff1ca37f9d6fc08791fd0641ebe8b94a7fce03f1
Danielius <dnlspasin@gmail.com>
2019-11-04 14:33:20 +0200

    follwoers "time ago" following

--- app/src/main/java/com/android/cameraapp/data/data_models/UserCollection.kt
+++ app/src/main/java/com/android/cameraapp/data/data_models/UserCollection.kt
@@ -18,7 +18,8 @@ sealed class UserCollection {
         val follower_name: String? = "N/A",
         val follower_uid: String? = "N/A",
         val followers_photo_url: String? = "N/A",
-        val when_followed: String? = "N/A"
+        val when_followed: String? = "N/A",
+        val following_time_long: Long? = 0
     )
     //SubCollection
 
