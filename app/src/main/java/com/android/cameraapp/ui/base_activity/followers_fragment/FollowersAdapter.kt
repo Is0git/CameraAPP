5947458960ef16e87f939e3be126e763d21fb6e7
Danielius <dnlspasin@gmail.com>
2019-11-04 10:33:42 +0200

    followers recyclerview and diffutil

--- app/src/main/java/com/android/cameraapp/ui/base_activity/followers_fragment/FollowersAdapter.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/followers_fragment/FollowersAdapter.kt
@@ -2,30 +2,60 @@ package com.android.cameraapp.ui.base_activity.followers_fragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.android.cameraapp.data.data_models.UserCollection
 import com.android.cameraapp.databinding.FollowersRecyclerviewBinding
 
-class FollowersAdapter : RecyclerView.Adapter<FollowersAdapter.MyViewHolder>() {
+class FollowersAdapter :
+    PagedListAdapter<UserCollection.Followers, FollowersAdapter.MyViewHolder>(callback) {
+    class MyViewHolder(val binding: FollowersRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding =
-            FollowersRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(
-            binding
-        )
     }
 
-    override fun getItemCount(): Int {
-        return 20
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = FollowersRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
+            val item = getItem(position)
+            holder.binding.item = item
     }
 
-    class MyViewHolder(val binding: FollowersRecyclerviewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
 
-    }
 }
+
+val callback = object : DiffUtil.ItemCallback<UserCollection.Followers>() {
+    override fun areItemsTheSame(
+        oldItem: UserCollection.Followers,
+        newItem: UserCollection.Followers
+    ): Boolean = oldItem.follower_uid == newItem.follower_uid
+
+    override fun areContentsTheSame(
+        oldItem: UserCollection.Followers,
+        newItem: UserCollection.Followers
+    ): Boolean = oldItem == newItem
+
+}
+//override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//    val binding =
+//        FollowersRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//    return MyViewHolder(
+//        binding
+//    )
+//}
+//
+//override fun getItemCount(): Int {
+//    return 20
+//}
+//
+//override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//
+//}
+//
+//class MyViewHolder(val binding: FollowersRecyclerviewBinding) :
+//    RecyclerView.ViewHolder(binding.root) {
+//
+//}
