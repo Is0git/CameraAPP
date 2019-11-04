bd0f1f3790a0e17635c7c0e2e31a09735a57e14b
Danielius <dnlspasin@gmail.com>
2019-11-04 15:54:08 +0200

    following data source factory

--- app/src/main/java/com/android/cameraapp/ui/base_activity/following_fragment/FollowingDataSource.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/following_fragment/FollowingDataSource.kt
@@ -3,6 +3,7 @@ package com.android.cameraapp.ui.base_activity.following_fragment
 import android.util.Log
 import androidx.paging.PositionalDataSource
 import com.android.cameraapp.data.data_models.UserCollection
+import com.android.cameraapp.di.base_activity.following_fragment.FollowingFragmentScope
 import com.android.cameraapp.ui.base_activity.followers_fragment.TAG
 import com.android.cameraapp.util.userCollection
 import com.android.cameraapp.util.userFollowingCollection
@@ -10,8 +11,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import javax.inject.Inject
 
-class FollowingDataSource constructor(val auth: FirebaseAuth, val firestore: FirebaseFirestore) :
+@FollowingFragmentScope
+class FollowingDataSource @Inject constructor(val auth: FirebaseAuth, val firestore: FirebaseFirestore) :
     PositionalDataSource<UserCollection.Following>() {
 
     var lastDocument: DocumentSnapshot? = null
---
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/following_fragment/FollowingDataSourceFactory.kt
@@ -0,0 +1,13 @@
+package com.android.cameraapp.ui.base_activity.following_fragment
+
+import androidx.paging.DataSource
+import com.android.cameraapp.data.data_models.UserCollection
+import com.android.cameraapp.di.base_activity.following_fragment.FollowingFragmentScope
+import javax.inject.Inject
+
+@FollowingFragmentScope
+class FollowingDataSourceFactory @Inject constructor(val dataSource:FollowingDataSource) : DataSource.Factory<Int, UserCollection.Following>() {
+    override fun create(): DataSource<Int, UserCollection.Following> {
+       return dataSource
+    }
+}
