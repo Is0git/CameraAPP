8c0afd523d0a5986170d3788c1f60250cad433bd
Danielius <dnlspasin@gmail.com>
2019-11-05 21:04:57 +0200

    Likes fragment viewmodelModule view multibinding key

---
+++ app/src/main/java/com/android/cameraapp/di/base_activity/likes_fragment/LikesFragmentViewModelModule.kt
@@ -0,0 +1,17 @@
+package com.android.cameraapp.di.base_activity.likes_fragment
+
+import androidx.lifecycle.ViewModel
+import com.android.cameraapp.di.ViewModelKey
+import com.android.cameraapp.ui.base_activity.likes_fragment.LikesFragmentViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class LikesFragmentViewModelModule {
+    @LikesFragmentScope
+    @Binds
+    @IntoMap
+    @ViewModelKey(LikesFragmentViewModel::class)
+    abstract fun getLikesFragmentViewmodel(viewmodel: LikesFragmentViewModel) : ViewModel
+}
