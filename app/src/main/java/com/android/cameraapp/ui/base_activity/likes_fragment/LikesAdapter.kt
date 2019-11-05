cc14381adedd39fa34f58a55b77ce6fa699e679f
Danielius <dnlspasin@gmail.com>
2019-11-05 21:17:01 +0200

    LikesFragment data binding

--- app/src/main/java/com/android/cameraapp/ui/base_activity/likes_fragment/LikesAdapter.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/likes_fragment/LikesAdapter.kt
@@ -2,30 +2,49 @@ package com.android.cameraapp.ui.base_activity.likes_fragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.android.cameraapp.data.data_models.DataFlat
 import com.android.cameraapp.databinding.LikesRecyclerviewBinding
 
-class LikesAdapter : RecyclerView.Adapter<LikesAdapter.MyViewHolder>() {
+class LikesAdapter : PagedListAdapter<DataFlat.Likes, LikesAdapter.MyViewHolder>() {
+     class MyViewHolder(val binding: LikesRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikesAdapter.MyViewHolder {
+                val binding =
             LikesRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyViewHolder(
             binding
         )
     }
 
-    override fun getItemCount(): Int {
-        return 20
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: LikesAdapter.MyViewHolder, position: Int) {
 
     }
 
-    class MyViewHolder(val binding: LikesRecyclerviewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
 
-    }
 }
+
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        val binding =
+//            LikesRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return MyViewHolder(
+//            binding
+//        )
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return 20
+//    }
+//
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//
+//    }
+//
+//    class MyViewHolder(val binding: LikesRecyclerviewBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//
+//    }
