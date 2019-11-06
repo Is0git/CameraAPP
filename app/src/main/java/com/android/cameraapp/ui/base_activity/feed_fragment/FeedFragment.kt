9d130a44865c45d6b3679c6e91507e3e87b93eec
Danielius <dnlspasin@gmail.com>
2019-11-06 20:32:32 +0200

    FeedFragment viewModel/ViewModelFactory and ui setup with observers

--- app/src/main/java/com/android/cameraapp/ui/base_activity/feed_fragment/FeedFragment.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/feed_fragment/FeedFragment.kt
@@ -5,16 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.android.cameraapp.databinding.FeedFragmentBinding
+import com.android.nbaapp.data.vms.ViewModelFactory
+import javax.inject.Inject
 
 class FeedFragment : Fragment() {
-        lateinit var binding: FeedFragmentBinding
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    @Inject
+    lateinit var adapter: PhotosWithUserAdapter
+    lateinit var viewModel: FeedFragmentViewModel
+    lateinit var binding: FeedFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(FeedFragmentViewModel::class.java)
         binding = FeedFragmentBinding.inflate(inflater, container, false)
+        binding.feedRecyclerView.adapter = adapter
+        viewModel.pagedList.observe(viewLifecycleOwner, Observer { adapter.submitList(it) })
         return binding.root
     }
 }
