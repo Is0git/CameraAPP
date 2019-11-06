ffa3ada100c41c0634d164c64481e81b57380d47
Danielius <dnlspasin@gmail.com>
2019-11-06 20:28:42 +0200

    Photos with users adapter

--- app/src/main/java/com/android/cameraapp/ui/base_activity/feed_fragment/PhotosWithUserAdapter.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/feed_fragment/PhotosWithUserAdapter.kt
@@ -1,13 +1,15 @@
 package com.android.cameraapp.ui.base_activity.feed_fragment
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.android.cameraapp.data.data_models.DataFlat
 import com.android.cameraapp.databinding.FeedListLayoutBinding
+import com.android.cameraapp.di.base_activity.feed_fragment.FeedFragmentScope
 import javax.inject.Inject
-
+@FeedFragmentScope
 class PhotosWithUserAdapter @Inject constructor() : PagedListAdapter<DataFlat.PhotosWithUser, PhotosWithUserAdapter.MyViewHolder>(
     callback) {
     class MyViewHolder(val binding: FeedListLayoutBinding) : RecyclerView.ViewHolder(binding.root)
@@ -16,11 +18,13 @@ class PhotosWithUserAdapter @Inject constructor() : PagedListAdapter<DataFlat.Ph
         parent: ViewGroup,
         viewType: Int
     ): PhotosWithUserAdapter.MyViewHolder {
-        ]
+        val binding = FeedListLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PhotosWithUserAdapter.MyViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       val item = getItem(position)
+        holder.binding.item = item
     }
 }
 
