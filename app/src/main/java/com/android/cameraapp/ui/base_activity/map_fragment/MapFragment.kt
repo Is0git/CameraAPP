1bfc1a0dae3c6cba415df405ace5bdef22caac13
Danielius <dnlspasin@gmail.com>
2019-11-18 15:33:11 +0200

    maps and current location

--- app/src/main/java/com/android/cameraapp/ui/base_activity/map_fragment/MapFragment.kt
+++ app/src/main/java/com/android/cameraapp/ui/base_activity/map_fragment/MapFragment.kt
@@ -10,20 +10,20 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.android.cameraapp.R
 import com.android.cameraapp.databinding.MapsFragmentBinding
-import com.google.android.gms.dynamic.ObjectWrapper
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.util.jar.Manifest
 
 const val permissionRequestCode = 205
 
 class MapFragment : Fragment() {
     lateinit var binding: MapsFragmentBinding
+    lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,24 +33,60 @@ class MapFragment : Fragment() {
         binding = MapsFragmentBinding.inflate(inflater, container, false)
 
 
-        if(checkPremissions()) generateMaps() else ActivityCompat.requestPermissions(activity!!, permissions, permissionRequestCode)
+        if (checkPremissions()) generateMaps() else ActivityCompat.requestPermissions(
+            activity!!,
+            permissions,
+            permissionRequestCode
+        )
 
         return binding.root
     }
 
-    fun checkPremissions() : Boolean = ContextCompat.checkSelfPermission(activity!!.applicationContext, permissions[0]) == PackageManager.PERMISSION_GRANTED
+    fun checkPremissions(): Boolean = ContextCompat.checkSelfPermission(
+        activity!!.applicationContext,
+        permissions[0]
+    ) == PackageManager.PERMISSION_GRANTED
 
     fun generateMaps() {
         val fragment = childFragmentManager.findFragmentById(R.id.map)
-        (fragment as SupportMapFragment) .getMapAsync(
+        (fragment as SupportMapFragment).getMapAsync(
             OnMapReadyCallback {
-                val sydney = LatLng(-33.852, 151.211)
-                it.addMarker(
-                    MarkerOptions().position(sydney)
-                        .title("Marker in Sydney")
-//                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.background))
+                if (it != null) {
+                    it.apply {
+                        isMyLocationEnabled = true
+                        uiSettings.isMyLocationButtonEnabled = true
+                    }
+//                    val sydney = LatLng(-33.852, 151.211)
+//                    it.addMarker(
+//                        MarkerOptions().position(sydney)
+//                            .title("Marker in Sydney")
+////                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.background))
+//                    )
+
+                    getDeviceLocation(it)
+                }
+            })
+    }
+
+    fun getDeviceLocation(map: GoogleMap) {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        val task = mFusedLocationProviderClient.lastLocation
+        task.addOnCompleteListener {
+            if (it.isSuccessful) {
+                val location = it.result
+                map.moveCamera(
+                    CameraUpdateFactory.newLatLng(
+                        LatLng(
+                            location?.altitude!!,
+                            location.longitude
+                        )
+                    )
                 )
-                it.moveCamera(CameraUpdateFactory.newLatLng(sydney)) })
+            } else {
+
+                map.uiSettings.isMyLocationButtonEnabled = false;
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -59,7 +95,7 @@ class MapFragment : Fragment() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == permissionRequestCode && checkPremissions()) generateMaps()
+        if (requestCode == permissionRequestCode && checkPremissions()) generateMaps()
     }
 
 }
